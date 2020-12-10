f835f302ad50e84ca59e274faccc20275483d202
Karla.Rodriguez <moriAeseces@gmail.com>
2020-12-09 19:36:31 -0600

    Se agrego CORS

--- server/server.js
+++ server/server.js
@@ -2,14 +2,31 @@ require('./config/config');
 const express = require('express');
 const mongoose = require ('mongoose');
 const bodyParser = require('body-parser');
+const cors = require ('cors');
 const bcrypt = require ('bcrypt');
 const app = express();
 
+// Habilita CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+  'Access-Control-Allow-Headers',
+  'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+  'Access-Control-Allow-Methods',
+  'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  );
+  next();
+  });
+
+  
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
  
 // parse application/json
 app.use(bodyParser.json());
+
 
 app.get('/', function (req, res) {
     res.send(' <h1>Bienvenido a mi servidor REST(localhost)</h1');
